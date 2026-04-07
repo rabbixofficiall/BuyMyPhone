99fecd9bd925033a64f63947ac09772c0edf6582
rabbixofficiall <rabbihossainltd@gmail.com>
2026-04-08 03:06:26 +0600

    Update BuyMyPhoneApp.kt

--- app/src/main/java/com/buymyphone/app/BuyMyPhoneApp.kt
+++ app/src/main/java/com/buymyphone/app/BuyMyPhoneApp.kt
@@ -2,13 +2,13 @@ package com.buymyphone.app
 
 import android.app.Application
 import androidx.multidex.MultiDex
+import com.google.android.gms.ads.MobileAds
 
 class BuyMyPhoneApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
-
-        // Future initialization (database, logging, etc.)
+        MobileAds.initialize(this)
     }
 
     override fun attachBaseContext(base: android.content.Context) {
