87660496499e8540f91313143572190e5fcbfc53
rabbixofficiall <rabbihossainltd@gmail.com>
2026-04-08 00:25:37 +0600

    Update BasicDeviceInfo.kt

--- app/src/main/java/com/buymyphone/app/model/BasicDeviceInfo.kt
+++ app/src/main/java/com/buymyphone/app/model/BasicDeviceInfo.kt
@@ -7,5 +7,13 @@ data class BasicDeviceInfo(
     val totalRamGb: Double,
     val availableRamGb: Double,
     val totalStorageGb: Double,
-    val availableStorageGb: Double
+    val availableStorageGb: Double,
+    val displayWidth: Int,
+    val displayHeight: Int,
+    val refreshRate: Float,
+    val densityDpi: Int,
+    val batteryLevelPercent: Int,
+    val batteryTemperatureCelsius: Float,
+    val isCharging: Boolean,
+    val batteryHealthText: String
 )
