bb315fe1371345543c43f7d1490b2c75091e1766
rabbixofficiall <rabbihossainltd@gmail.com>
2026-04-07 23:58:11 +0600

    Create SplashActivity.kt

---
+++ app/src/main/java/com/buymyphone/app/ui/splash/SplashActivity.kt
@@ -0,0 +1,25 @@
+package com.buymyphone.app.ui.splash
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.buymyphone.app.databinding.ActivitySplashBinding
+import com.buymyphone.app.ui.home.HomeActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, HomeActivity::class.java))
+            finish()
+        }, 1800)
+    }
+}
