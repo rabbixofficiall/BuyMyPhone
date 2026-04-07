f109bb413f6f29727c390435a83d242a0a683415
rabbixofficiall <rabbihossainltd@gmail.com>
2026-04-08 01:17:11 +0600

    Update gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -1,10 +1,51 @@
-@echo off
-set DIR=%~dp0
-set WRAPPER_JAR=%DIR%\gradle\wrapper\gradle-wrapper.jar
+@rem
+@rem Gradle startup script for Windows
+@rem
 
-if not exist "%WRAPPER_JAR%" (
-  echo Missing gradle-wrapper.jar
-  exit /b 1
-)
+@if "%DEBUG%"=="" @echo off
+@setlocal
 
-java -jar "%WRAPPER_JAR%" %*
+set DIRNAME=%~dp0
+if "%DIRNAME%"=="" set DIRNAME=.
+set APP_BASE_NAME=%~n0
+set APP_HOME=%DIRNAME%
+
+set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
+
+if defined JAVA_HOME goto findJavaFromJavaHome
+
+set JAVA_EXE=java.exe
+%JAVA_EXE% -version >NUL 2>&1
+if "%ERRORLEVEL%" == "0" goto execute
+
+echo.
+echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
+echo.
+echo Please set the JAVA_HOME variable in your environment to match the
+echo location of your Java installation.
+goto fail
+
+:findJavaFromJavaHome
+set JAVA_HOME=%JAVA_HOME:"=%
+set JAVA_EXE=%JAVA_HOME%/bin/java.exe
+
+if exist "%JAVA_EXE%" goto execute
+
+echo.
+echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME%
+echo.
+echo Please set the JAVA_HOME variable in your environment to match the
+echo location of your Java installation.
+goto fail
+
+:execute
+set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
+
+"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
+
+:end
+@endlocal
+goto :eof
+
+:fail
+exit /b 1
