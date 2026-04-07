1dbb8164a99a4b24799e5ecfe43a96aef7cc0246
rabbixofficiall <rabbihossainltd@gmail.com>
2026-04-08 03:09:41 +0600

    Update HomeActivity.kt

--- app/src/main/java/com/buymyphone/app/ui/home/HomeActivity.kt
+++ app/src/main/java/com/buymyphone/app/ui/home/HomeActivity.kt
@@ -3,9 +3,12 @@ package com.buymyphone.app.ui.home
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.buymyphone.app.R
+import com.buymyphone.app.ads.AdManager
 import com.buymyphone.app.databinding.ActivityHomeBinding
 import com.buymyphone.app.ui.about.AboutActivity
 import com.buymyphone.app.ui.analysis.AnalysisActivity
+import com.buymyphone.app.ui.premium.PremiumActivity
 import com.buymyphone.app.utils.AnimationUtils
 
 class HomeActivity : AppCompatActivity() {
@@ -20,6 +23,9 @@ class HomeActivity : AppCompatActivity() {
         AnimationUtils.fadeIn(this, binding.txtTitle)
         AnimationUtils.slideUp(this, binding.cardMain)
 
+        val adView = AdManager.createBannerAd(this, getString(R.string.admob_banner_home))
+        AdManager.attachBanner(binding.adContainerHome, adView)
+
         binding.btnStartAnalysis.setOnClickListener {
             AnimationUtils.pulse(this, binding.btnStartAnalysis)
             startActivity(Intent(this, AnalysisActivity::class.java))
@@ -27,6 +33,10 @@ class HomeActivity : AppCompatActivity() {
 
         binding.btnAboutApp.setOnClickListener {
             startActivity(Intent(this, AboutActivity::class.java))
+        }
+
+        binding.btnGoPremium.setOnClickListener {
+            startActivity(Intent(this, PremiumActivity::class.java))
         }
     }
 }
