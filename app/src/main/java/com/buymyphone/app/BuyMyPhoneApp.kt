b8fab1e4cb4a182436476a47abec15f54daec945
rabbixofficiall <rabbihossainltd@gmail.com>
2026-04-08 21:00:51 +0600

    Update BuyMyPhoneApp.kt

--- app/src/main/java/com/buymyphone/app/BuyMyPhoneApp.kt
+++ app/src/main/java/com/buymyphone/app/BuyMyPhoneApp.kt
@@ -1,13 +1,16 @@
 package com.buymyphone.app
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.multidex.MultiDex
+import com.buymyphone.app.settings.SettingsManager
 import com.google.android.gms.ads.MobileAds
 
 class BuyMyPhoneApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        SettingsManager.applyTheme(this)
         MobileAds.initialize(this)
     }
 
