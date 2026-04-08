b09e84d650480ee58b44502a6da66dcb94e0e75d
rabbixofficiall <rabbihossainltd@gmail.com>
2026-04-08 14:49:56 +0600

    Update HomeActivity.kt

--- app/src/main/java/com/buymyphone/app/ui/home/HomeActivity.kt
+++ app/src/main/java/com/buymyphone/app/ui/home/HomeActivity.kt
@@ -4,12 +4,10 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.buymyphone.app.R
-import com.buymyphone.app.ads.AdManager
 import com.buymyphone.app.databinding.ActivityHomeBinding
-import com.buymyphone.app.ui.about.AboutActivity
 import com.buymyphone.app.ui.analysis.AnalysisActivity
+import com.buymyphone.app.ui.about.AboutActivity
 import com.buymyphone.app.ui.premium.PremiumActivity
-import com.buymyphone.app.utils.AnimationUtils
 
 class HomeActivity : AppCompatActivity() {
 
@@ -20,23 +18,19 @@ class HomeActivity : AppCompatActivity() {
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        AnimationUtils.fadeIn(this, binding.txtTitle)
-        AnimationUtils.slideUp(this, binding.cardMain)
-
-        val adView = AdManager.createBannerAd(this, getString(R.string.admob_banner_home))
-        AdManager.attachBanner(binding.adContainerHome, adView)
-
         binding.btnStartAnalysis.setOnClickListener {
-            AnimationUtils.pulse(this, binding.btnStartAnalysis)
             startActivity(Intent(this, AnalysisActivity::class.java))
+            overridePendingTransition(R.anim.fade_slide, android.R.anim.fade_out)
         }
 
         binding.btnAboutApp.setOnClickListener {
             startActivity(Intent(this, AboutActivity::class.java))
+            overridePendingTransition(R.anim.fade_slide, android.R.anim.fade_out)
         }
 
         binding.btnGoPremium.setOnClickListener {
             startActivity(Intent(this, PremiumActivity::class.java))
+            overridePendingTransition(R.anim.fade_slide, android.R.anim.fade_out)
         }
     }
 }
